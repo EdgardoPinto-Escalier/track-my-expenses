e555f2af407b756a7725455ab2a93b588a98d2c7
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-02-24 22:04:39 +0100

    Readme file updated

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Expense Tracker made with React
 
 - React Hooks
 - Context API
+
+Demo<br />
+Open [Track My Expenses](https://track-my-expenses.netlify.com/) to view it in the browser.
---
+++ build/precache-manifest.38bc5d0a555ab1a1c9690c23bcde4f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d88ed5646fcb1434fe7e227f8eea4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8b61ad286e6ba934212",
+    "url": "/static/css/main.02ce3edf.chunk.css"
+  },
+  {
+    "revision": "fb5cd85cd8a1b9f7f8f6",
+    "url": "/static/js/2.ddc7a53a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ddc7a53a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b61ad286e6ba934212",
+    "url": "/static/js/main.34eac3d0.chunk.js"
+  },
+  {
+    "revision": "d7a5fe73f1ca363349f4",
+    "url": "/static/js/runtime-main.9ab6e02c.js"
+  }
+]);
